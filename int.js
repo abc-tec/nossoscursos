a2110d5f50606753ef2dc8903c4cde3f6e5c468d
abc-tec <abcinstrutores@gmail.com>
2026-03-13 14:44:25 -0300

    Add files via upload

--- int.js
+++ int.js
@@ -1,4 +1,28 @@
+const cacheName = 'meu-site-v1';
+const assets = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js'
+];
 
+// Instala o service worker e guarda os arquivos no cache
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Responde com o cache mesmo se estiver sem internet
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
 const observer = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
             if (entry.isIntersecting) {
